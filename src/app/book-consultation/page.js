c8efde97c3eaef9111045d929cfcd89e0576f5e6
Akinfenwa Taiwo Christopher <101477417+AChris-T@users.noreply.github.com>
2025-05-26 10:12:19 +0100

    feat:added the home page

---
+++ src/app/book-consultation/page.js
@@ -0,0 +1,143 @@
+'use client';
+import React from 'react';
+import { motion } from 'framer-motion';
+import Header from '@/components/Header';
+import FacebookIcon from '../../../public/icons/FacebookIcon';
+import TwitterIcon from '../../../public/icons/TwitterIcon';
+import LinkdinIcon from '../../../public/icons/LinkdinIcon';
+import Stepper from '@/components/consultaionForm/Stepper';
+
+const fadeInUp = {
+  initial: { opacity: 0, y: 20 },
+  animate: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.6,
+      ease: 'easeOut',
+    },
+  },
+};
+
+export default function page() {
+  return (
+    <div className="overflow-hidden">
+      <motion.div initial="initial" animate="animate" variants={fadeInUp}>
+        <Header />
+      </motion.div>
+      <div className="py-[75px] px-2 md:px-[39px] gap-[10px] flex flex-col">
+        {/*    <Script
+        src="https://assets.calendly.com/assets/external/widget.js"
+        strategy="beforeInteractive"
+      /> */}
+        <div className="flex w-full items-start  justify-between  flex-col lg:flex-row mt-[30px] md:mt-[85px] gap-[30px] ">
+          <div className="flex flex-col justify-center gap-[50px]  ">
+            <h3 className="text-[30px] flex flex-col md:text-[80px] leading-12  w-full font-semibold md:leading-24 text-shade-100 lg:w-[647px] md:text-start  text-center lg:text-start ">
+              Your Next
+              <p>Big Opportunity </p> Starts Here! 🚀
+            </h3>
+            <div className="lg:grid grid-cols-1 hidden px-3 md:grid-cols-2 justify-center gap-8 md:gap-20 md:mx-20 lg:mx-0">
+              <div className="flex flex-col gap-4">
+                <h3 className="font-semibold text-[24px] text-black">
+                  Direct Contact
+                </h3>
+                <h3 className="flex gap-3 items-center font-semibold text-base text-black">
+                  Email:
+                  <span className="text-green-100 font-normal">
+                    collab@viridusgreen.co
+                  </span>
+                </h3>
+                <h3 className="flex gap-8 items-center font-semibold text-base text-black">
+                  Tel:
+                  <span className="text-green-100 font-normal">
+                    +44 20 7946 XXXX
+                  </span>
+                </h3>
+                <h3 className="flex gap-3 items-start font-semibold text-base text-black">
+                  Office:
+                  <span className="text-green-100 w-full md:w-[163px] font-normal">
+                    123 Business Centre, Innovation Park, London, EC2A 1NT,
+                    United Kingdom{' '}
+                  </span>
+                </h3>
+              </div>
+              <div className="flex flex-col gap-4 ]">
+                <h3 className="font-semibold text-[24px] text-black">
+                  Social Media
+                </h3>
+                <h3 className="flex gap-3 items-start font-semibold text-base text-green-100">
+                  <FacebookIcon />
+                  <span className="text-black-400 w-[163px] font-normal">
+                    @viridusgreen
+                  </span>
+                </h3>
+                <h3 className="flex gap-3 items-start font-semibold text-base text-green-100">
+                  <TwitterIcon />
+                  <span className="text-black-400 w-[163px] font-normal">
+                    @viridusgreen.co{' '}
+                  </span>
+                </h3>
+                <h3 className="flex gap-3 items-start font-semibold text-base text-green-100">
+                  <LinkdinIcon />
+                  <span className="text-black-400 w-[163px] font-normal">
+                    @viridusgreen
+                  </span>
+                </h3>
+              </div>
+            </div>
+          </div>
+          <Stepper />
+        </div>
+        <div className="grid   grid-cols-1 lg:hidden px-3 md:grid-cols-2 justify-center gap-8 md:gap-20 w-full mt-10 lg:mx-0">
+          <div className="flex flex-col gap-4">
+            <h3 className="font-semibold text-[20px] text-green-100">
+              Direct Contact
+            </h3>
+            <h3 className="flex gap-3 items-center font-semibold text-base text-green-100">
+              Email:
+              <span className="text-black-400 font-normal">
+                collab@viridusgreen.co
+              </span>
+            </h3>
+            <h3 className="flex gap-8 items-center font-semibold text-base text-green-100">
+              Tel:
+              <span className="text-black-400 font-normal">
+                +44 20 7946 XXXX
+              </span>
+            </h3>
+            <h3 className="flex gap-3 items-start font-semibold text-base text-green-100">
+              Office:
+              <span className="text-black-400 w-full  font-normal">
+                123 Business Centre, Innovation Park, London, EC2A 1NT, United
+                Kingdom{' '}
+              </span>
+            </h3>
+          </div>
+          <div className="flex flex-col gap-4 ]">
+            <h3 className="font-semibold text-[20px] text-green-100">
+              Social Media
+            </h3>
+            <h3 className="flex gap-3 items-start font-semibold text-base text-green-100">
+              <FacebookIcon />
+              <span className="text-black-400 w-[163px] font-normal">
+                @viridusgreen
+              </span>
+            </h3>
+            <h3 className="flex gap-3 items-start font-semibold text-base text-green-100">
+              <TwitterIcon />
+              <span className="text-black-400 w-[163px] font-normal">
+                @viridusgreen.co{' '}
+              </span>
+            </h3>
+            <h3 className="flex gap-3 items-start font-semibold text-base text-green-100">
+              <LinkdinIcon />
+              <span className="text-black-400 w-[163px] font-normal">
+                @viridusgreen
+              </span>
+            </h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/consultaionForm/FormStep1.jsx
@@ -0,0 +1,116 @@
+'use client';
+import { ErrorMessage, Field, useFormikContext } from 'formik';
+import React, { useState } from 'react';
+
+export default function FormStep1() {
+  const [showSecondForm, setShowSecondForm] = useState(false);
+  const [showTechnologyForm, setShowTechnologyForm] = useState(false);
+  const { setFieldValue } = useFormikContext(); // Formik helper to update values
+
+  return (
+    <div className="flex flex-col gap-4">
+      <h3 className="text-black mb-4 font-semibold">SERVICE TYPE</h3>
+
+      {/* Service Type Select */}
+      <div className="flex flex-col gap-2">
+        <label className=" text-black font-medium">Service Type</label>
+        <div className="border-black-400 border rounded-xl p-4">
+          <Field
+            name="serviceType"
+            as="select"
+            onChange={(e) => {
+              const selected = e.target.value;
+              setFieldValue('serviceType', selected);
+              setShowSecondForm(selected === 'Bid-Writing Consulting');
+              setShowTechnologyForm(selected === 'Technology Delivery');
+            }}
+            className="w-full text-green-100 h-full focus:outline-none"
+          >
+            <option value="">Select Option</option>
+            <option value="Bid-Writing Consulting">
+              Bid-Writing Consulting
+            </option>
+            <option value="Sustainability Consulting">
+              Sustainability Consulting
+            </option>
+            <option value="Commodity Management">Commodity Management</option>
+            <option value="Technology Delivery">Technology Delivery</option>
+          </Field>
+        </div>
+        <ErrorMessage
+          name="serviceType"
+          component="div"
+          className="text-red-500 text-sm"
+        />
+      </div>
+
+      {/* Bid-Writing Sector */}
+      {showSecondForm && (
+        <div className="flex flex-col gap-2">
+          <label className="text-green-100 font-medium">
+            Bid-Writing Sector
+          </label>
+          <div className="border-black-400 border rounded-xl p-4">
+            <Field
+              as="select"
+              name="BidWritingSector"
+              className="w-full text-green-100 h-full focus:outline-none"
+            >
+              <option value="">Select Option</option>
+              <option value="Construction">Construction</option>
+              <option value="Facilities Management ">
+                Facilities Management
+              </option>
+              <option value="Healthcare and Social Care">
+                Healthcare and Social Care
+              </option>
+              <option value="Education & youth services">
+                Education & youth services
+              </option>
+              <option value="Education & youth services">
+                Education & youth services
+              </option>
+              <option value="Commercial and Domestic Cleaning services">
+                Commercial and Domestic Cleaning services
+              </option>
+              <option value="Charity">Charity</option>
+              <option value="others">others </option>
+            </Field>
+          </div>
+          <ErrorMessage
+            name="BidWritingSector"
+            component="div"
+            className="text-red-500 text-sm"
+          />
+        </div>
+      )}
+
+      {/* Technology Delivery Type */}
+      {showTechnologyForm && (
+        <div className="flex flex-col gap-2">
+          <label className="text-green-100 font-medium">Technology Type</label>
+          <div className="border-black-400 border rounded-xl p-4">
+            <Field
+              as="select"
+              name="TechnologyType"
+              className="w-full text-green-100 h-full focus:outline-none"
+            >
+              <option value="">Select a service</option>
+              <option value="App Development">App Development</option>
+              <option value="Data Analytics">Data Analytics</option>
+              <option value="Website Development">Website Development</option>
+              <option value="Cloud Migration">Cloud Migration</option>
+              <option value="Process Automation">Process Automation</option>
+              <option value="Software Development">Software Development</option>
+            </Field>
+          </div>
+          <ErrorMessage
+            name="TechnologyType"
+            component="div"
+            className="text-red-500 text-sm"
+          />
+        </div>
+      )}
+    </div>
+  );
+}
